1cc628192a544337ef71b7ce5df9916798cc58e4
AngieDutra <angiedutraa@gmail.com>
2021-10-19 20:43:34 -0300

    Primer commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+// Express
+const express = require('express');
+const app = express();
+
+// Paths
+const path = require('path');
+
+// Server
+app.set("port", process.env.PORT || 3000);
+app.listen(app.get('port'), ()=> console.log('Server en http://localhost:' + app.get('port')));
+
+// Settings
+    // Public folder
+app.use(express.static(path.resolve(__dirname, '../public'))); 
+    // Views folder
+app.set("views", path.resolve(__dirname, "./views"));
+    // View Engine
+app.set("view engine", "ejs");
+
+// Routes
+    // Index
+    const mainRoutes = require('./routes/mainRoutes.js');
+    app.use('/', mainRoutes);
